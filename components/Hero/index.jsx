a0db0dce37295368311d3bf9baf87a5f13d056a3
Sevi <correosevi@gmail.com>
2021-06-22 14:48:39 +0100

    Add styles to hero banner

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -4,8 +4,19 @@ import WrapperFlex from '../Layouts/WrapperFlex';
 const Hero = () => {
   return (
     <WrapperFlex background="#fdcd07" minheight="50vh">
-      <div className="text">Main text</div>
-      <div className="picture">Picture</div>
+      <div className={styles.presentation}>
+        <h1>Welcome!</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          suscipit expedita sint, quia in obcaecati cumque, saepe maiores nemo
+          fuga libero quaerat porro non laboriosam cupiditate, quas quod.
+          Distinctio, voluptatem. Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Voluptatibus suscipit expedita sint, quia in
+          obcaecati cumque, saepe maiores nemo fuga libero quaerat porro non
+          laboriosam cupiditate, quas quod. Distinctio, voluptatem.
+        </p>
+      </div>
+      <div className={styles.picture}>Picture</div>
     </WrapperFlex>
   );
 };
