dae681a41db26cbbc886f0c0b293d317c8e7e865
Sevi <correosevi@gmail.com>
2021-06-24 16:23:03 +0100

    Create styles for the mobile nav button

--- components/Header/Navigation/index.jsx
+++ components/Header/Navigation/index.jsx
@@ -5,6 +5,25 @@ const Navigation = () => {
   return (
     <div className={styles.navwrapper}>
       <h1 className={styles.logo}>Izzi Howell</h1>
+      <div className={styles.mobilenav}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-menu-2"
+          width="44"
+          height="44"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="#2a2b57"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <line x1="4" y1="6" x2="20" y2="6" />
+          <line x1="4" y1="12" x2="20" y2="12" />
+          <line x1="4" y1="18" x2="20" y2="18" />
+        </svg>
+      </div>
       <nav className={styles.navigation}>
         <ul>
           <li>Home</li>
