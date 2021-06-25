b0e34fb361d4d6824389e55795b588c211768633
Sevi <correosevi@gmail.com>
2021-06-25 14:25:33 +0100

    Create a title section inside the book component

--- components/Books/index.jsx
+++ components/Books/index.jsx
@@ -1,77 +1,85 @@
 import styles from './Books.module.scss';
+import WrapperFlex from '../Layouts/WrapperFlex/';
 
 const Books = () => {
   return (
-    <section className={styles.wrapper}>
-      <div className={styles.book}>
-        <img
-          src="books/cats-react-scient.png"
-          alt="Cats react to scient facts"
-        />
-        <h3>Cats react to scient facts</h3>
-      </div>
-      <div className={styles.book}>
-        <img
-          src="books/cats-react-scient.png"
-          alt="Cats react to scient facts"
-        />
-        <h3>Cats react to scient facts</h3>
-      </div>
-      <div className={styles.book}>
-        <img
-          src="books/cats-react-scient.png"
-          alt="Cats react to scient facts"
-        />
-        <h3>Cats react to scient facts</h3>
-      </div>
-      <div className={styles.book}>
-        <img
-          src="books/cats-react-scient.png"
-          alt="Cats react to scient facts"
-        />
-        <h3>Cats react to scient facts</h3>
-      </div>
-      <div className={styles.book}>
-        <img
-          src="books/cats-react-scient.png"
-          alt="Cats react to scient facts"
-        />
-        <h3>Cats react to scient facts</h3>
-      </div>
-      <div className={styles.book}>
-        <img
-          src="books/cats-react-scient.png"
-          alt="Cats react to scient facts"
-        />
-        <h3>Cats react to scient facts</h3>
-      </div>
-      <div className={styles.book}>
-        <img
-          src="books/cats-react-scient.png"
-          alt="Cats react to scient facts"
-        />
-        <h3>Cats react to scient facts</h3>
-      </div>
-      <div className={styles.book}>
-        <img
-          src="books/cats-react-scient.png"
-          alt="Cats react to scient facts"
-        />
-        <h3>Cats react to scient facts</h3>
-      </div>
-      <div className={styles.book}>
-        <img
-          src="books/cats-react-scient.png"
-          alt="Cats react to scient facts"
-        />
-        <h3>Cats react to scient facts</h3>
-      </div>
-      <div className={styles.book}>
-        <img
-          src="books/cats-react-scient.png"
-          alt="Cats react to scient facts"
-        />
-        <h3>Cats react to scient facts</h3>
+    <section>
+      <WrapperFlex>
+        <h1 className={styles.sectiontitle}>My books</h1>
+        <p className={styles.sectionsubtitle}>A showcase of some of my books</p>
+      </WrapperFlex>
+
+      <div className={styles.gridwrapper}>
+        <div className={styles.book}>
+          <img
+            src="books/cats-react-scient.png"
+            alt="Cats react to scient facts"
+          />
+          <h3>Cats react to scient facts</h3>
+        </div>
+        <div className={styles.book}>
+          <img
+            src="books/cats-react-scient.png"
+            alt="Cats react to scient facts"
+          />
+          <h3>Cats react to scient facts</h3>
+        </div>
+        <div className={styles.book}>
+          <img
+            src="books/cats-react-scient.png"
+            alt="Cats react to scient facts"
+          />
+          <h3>Cats react to scient facts</h3>
+        </div>
+        <div className={styles.book}>
+          <img
+            src="books/cats-react-scient.png"
+            alt="Cats react to scient facts"
+          />
+          <h3>Cats react to scient facts</h3>
+        </div>
+        <div className={styles.book}>
+          <img
+            src="books/cats-react-scient.png"
+            alt="Cats react to scient facts"
+          />
+          <h3>Cats react to scient facts</h3>
+        </div>
+        <div className={styles.book}>
+          <img
+            src="books/cats-react-scient.png"
+            alt="Cats react to scient facts"
+          />
+          <h3>Cats react to scient facts</h3>
+        </div>
+        <div className={styles.book}>
+          <img
+            src="books/cats-react-scient.png"
+            alt="Cats react to scient facts"
+          />
+          <h3>Cats react to scient facts</h3>
+        </div>
+        <div className={styles.book}>
+          <img
+            src="books/cats-react-scient.png"
+            alt="Cats react to scient facts"
+          />
+          <h3>Cats react to scient facts</h3>
+        </div>
+        <div className={styles.book}>
+          <img
+            src="books/cats-react-scient.png"
+            alt="Cats react to scient facts"
+          />
+          <h3>Cats react to scient facts</h3>
+        </div>
+        <div className={styles.book}>
+          <img
+            src="books/cats-react-scient.png"
+            alt="Cats react to scient facts"
+          />
+          <h3>Cats react to scient facts</h3>
+        </div>
       </div>
     </section>
   );
