d5c6fc89f92303b4e30ebc557593d3a1796a4d7f
Sevi <correosevi@gmail.com>
2021-06-25 10:14:10 +0100

    Create portfolio (Books) component

---
+++ components/Books/index.js
@@ -0,0 +1,7 @@
+import styles from './Books.module.scss';
+
+const Books = () => {
+  return <section className={styles.wrapper}></section>;
+};
+
+export default Books;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import styles from '../styles/Home.module.css';
 import Header from '../components/Header';
+import Books from '../components/Books';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Books />
+      </main>
 
       <footer className={styles.footer}></footer>
     </div>
