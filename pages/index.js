e36df744ecd4ccc9ac68346e3efa34b5147546c1
Sevi <correosevi@gmail.com>
2021-06-27 14:59:31 +0100

    Award section discarted by customer

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
 import styles from '../styles/Home.module.css';
 import Header from '../components/Header';
 import Books from '../components/Books';
-import Award from '../components/Award';
+
 import WrapperFlex from '../components/Layouts/WrapperFlex';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <WrapperFlex>
-        <Award />
-      </WrapperFlex>
+      <WrapperFlex></WrapperFlex>
       <Books />
 
       <footer className={styles.footer}></footer>
