0e66cef9f2d19a93a645d640f4879256b8963d0d
Sevi <correosevi@gmail.com>
2021-07-06 20:07:00 +0100

    Clean and refactory some general styles

--- components/Instagram/index.jsx
+++ components/Instagram/index.jsx
@@ -3,12 +3,12 @@ import WrapperGrid from '../Layouts/WrapperGrid';
 import WrapperFlex from '../Layouts/WrapperFlex';
 
 const Instagram = ({ feed }) => {
-  const postsNumber = feed.filter((post, index) => index < 5);
+  const postsNumber = feed.filter((post, index) => index < 6);
   return (
     <section className={styles.container}>
-      <WrapperFlex background="grey" align="left">
-        <p className={styles.sectionsubtitle}>My latest posts</p>
+      <WrapperFlex background="grey">
         <h1 className={styles.sectiontitle}>Instagram</h1>
+        <p className={styles.sectionsubtitle}>My latest posts</p>
       </WrapperFlex>
       <WrapperGrid background="grey">
         {postsNumber.map((post, index) => (
