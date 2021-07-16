60597cb4edbe5c88aed0ecb30cbf5b257ccfac7c
Sevi <correosevi@gmail.com>
2021-07-16 19:43:08 +0100

    Add new data array for reviews

---
+++ data/reviews.js
@@ -0,0 +1,17 @@
+export const rewiews = [
+  {
+    id: 1,
+    text: '“ Cats reacts to scient facts makes science fun and engaging by breaking down difficult information in a relatable way. The book introduces topics like forces, energy and climate change — the real fundamentals — in a fun and engaging way. It was thoroughly road tested in my house, it wasn’t a quick scan, this was the book my kids wanted to take to bed with them and flick through.”',
+    author: 'Professor Rosalind Rickaby,Judge and Royal Society Research',
+  },
+  {
+    id: 2,
+    text: '“ Cats reacts to scient facts makes science fun and engaging by breaking down difficult information in a relatable way. The book introduces topics like forces, energy and climate change — the real fundamentals — in a fun and engaging way. It was thoroughly road tested in my house, it wasn’t a quick scan, this was the book my kids wanted to take to bed with them and flick through.”',
+    author: 'Another person',
+  },
+  {
+    id: 3,
+    text: '“ Cats reacts to scient facts makes science fun and engaging by breaking down difficult information in a relatable way. The book introduces topics like forces, energy and climate change — the real fundamentals — in a fun and engaging way. It was thoroughly road tested in my house, it wasn’t a quick scan, this was the book my kids wanted to take to bed with them and flick through.”',
+    author: 'The 3º person',
+  },
+];
