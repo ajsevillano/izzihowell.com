c0a78d95b2f3d1ddc1c809a31f1855a574e843f1
Sevi <correosevi@gmail.com>
2021-07-17 19:45:56 +0100

    wrap the section and map for data

--- components/Reviews/index.jsx
+++ components/Reviews/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import SectionContainer from '../Layouts/SectionContainer';
 import WrapperFlex from '../Layouts/WrapperFlex';
 import styles from './Reviews.module.scss';
@@ -9,26 +9,24 @@ const Reviews = () => {
 
   return (
     <SectionContainer background="blue">
-      <WrapperFlex>
-        <h1 className={styles.title}>Some reviews about my books</h1>
-        <img
-          className={styles.reviewimages}
-          src="ros_rickaby.jpg"
-          alt="Professor Rosalind Rickaby"
-        />
-        <p className={styles.subtitle}>
-          “<span className={styles.bold}> Cats reacts to scient facts</span>{' '}
-          makes science fun and engaging by breaking down difficult information
-          in a relatable way. The book introduces topics like forces, energy and
-          climate change — the real fundamentals — in a fun and engaging way. It
-          was thoroughly road tested in my house, it wasn’t a quick scan, this
-          was the book my kids wanted to take to bed with them and flick
-          through.”
-        </p>
-        <p className={styles.reviewname}>
-          Professor Rosalind Rickaby,Judge and Royal Society Research
-        </p>
-      </WrapperFlex>
+      {reviews.map(
+        (data) =>
+          data.id === 1 && (
+            <WrapperFlex key={data.id}>
+              <h1 className={styles.title}>Some reviews about my books</h1>
+              <img
+                className={styles.reviewimages}
+                src={data.img}
+                alt={data.author}
+              />
+              <p className={styles.subtitle}>
+                “<span className={styles.bold}>{data.title}</span>
+                {data.text}
+              </p>
+              <p className={styles.reviewname}>{data.author}</p>
+            </WrapperFlex>
+          )
+      )}
     </SectionContainer>
   );
 };
