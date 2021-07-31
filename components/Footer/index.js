b399bb75df1c4a8f9a237c90f8a65e9d18efb1c5
Sevi <correosevi@gmail.com>
2021-07-31 19:43:20 +0100

    Add logo on footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -7,6 +7,7 @@ const Footer = () => {
     <SectionContainer background="dark-blue">
       <WrapperFlex align="align-center">
         <h1 className={styles.title}>Contact me</h1>
+        <h1 className={styles.logo}>Izzi Howell</h1>
       </WrapperFlex>
     </SectionContainer>
   );
